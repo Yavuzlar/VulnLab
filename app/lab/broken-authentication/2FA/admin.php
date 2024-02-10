4c76fb11eb9d5127b45aabeb9c260d0fbd12c8a3
Özgür Karakuş <103831584+ozgur-karakus@users.noreply.github.com>
2024-02-10 18:39:19 +0300

    revise

--- app/lab/broken-authentication/2FA/admin.php
+++ app/lab/broken-authentication/2FA/admin.php
@@ -7,6 +7,12 @@ if ($_SESSION['username'] == '') {
     header('Location: index.php');
     exit();
 }
+if (isset($_POST['logout'])) {
+    session_unset();
+    session_destroy();
+    header('Location: index.php');
+    exit();
+}
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -18,7 +24,7 @@ if ($_SESSION['username'] == '') {
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
 </head>
 <body class="bg-light">
-    <div class="container mt-5"style="padding-top:5%;">
+    <div class="container mt-5" style="padding-top:5%;">
         <div class="row justify-content-center">
             <div class="col-md-6">
                 <div class="card mx-auto">
@@ -34,6 +40,9 @@ if ($_SESSION['username'] == '') {
                         </div>
                     </div>
                     <div class="card-footer text-muted text-center">
+                        <form method="post" action="">
+                            <button type="submit" name="logout" class="btn btn-danger"><?php echo $strings["logout"]; ?></button>
+                        </form>
                         &copy; <?php echo $strings["bos"]; ?>
                     </div>
                 </div>
