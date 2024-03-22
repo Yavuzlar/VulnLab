7e1c77b97bbe20d3459663d81b7474d4f5826e05
Murat <muraatbulut35@gmail.com>
2024-03-22 15:05:40 +0300

    path traversal added.

---
+++ app/lab/pathtraversal/pathtraversal-2/index.php
@@ -0,0 +1,66 @@
+<?php require("../../../lang/lang.php"); 
+$lang = tr();
+?>
+<!DOCTYPE html>
+<html lang="tr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <title><?php echo $lang['title']; ?></title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <style>
+        .urun-karti {
+            margin-bottom: 20px;
+            cursor: pointer;
+        }
+
+        .urun-karti img {
+            max-width: 100%;
+            height: auto;
+        }
+
+        .urun-karti-baslik {
+            margin-top: 10px;
+        }
+    </style>
+</head>
+<body>
+    <div class="container mt-5">
+        <div class="row">
+            <?php
+                $imageCount = 4;
+
+                for ($i = 1; $i <= $imageCount; $i++) {
+                    $productId = $i;
+                    $imageName = $i . ".png";
+
+                    echo '<div class="col-md-3">';
+                    echo '<div class="urun-karti">';
+                    echo '<a href="product-detail.php?productId=' . $productId . '.png' . '">';
+                    echo '<img src="img/' . $imageName . '">';
+                    echo '<h4 class="urun-karti-baslik">';
+                    switch ($productId) {
+                        case 1:
+                            echo $lang['productTitle1'];
+                            break;
+                        case 2:
+                            echo $lang['productTitle2'];
+                            break;    
+                        case 3:
+                            echo $lang['productTitle3'];
+                            break;
+                        case 4:
+                            echo $lang['productTitle4'];
+                            break;
+                    }
+                    echo '</h4>';
+                    echo '</a>';
+                    echo '</div>';
+                    echo '</div>';
+                }
+            ?>
+        </div>
+    </div>
+    <script id="VLBar" title="<?= $lang["title"]; ?>" category-id="9" src="/public/assets/js/vlnav.min.js"></script>
+</body>
+</html>
